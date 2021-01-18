2f4c9629748c542592e2834c3cb655426c185bb7
aznjp <jpark103193@gmail.com>
2021-01-18 14:58:31 -0500

    add in tests for testing out actions for updating state

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,8 @@
+// original state
+const state = {
+    name: 'Lernantino',
+    email: 'lernantino@gmail.com' 
+  }
+  
+  // update (or mutate) state directly
+  state.email = 'lernantino99@gmail.com';
